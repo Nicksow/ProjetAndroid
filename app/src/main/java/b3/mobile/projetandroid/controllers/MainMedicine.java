e1913f8c6f05197fa7ba15533bc075584ad25dff
Nicksow <nick93736@gmail.com>
2023-10-17 10:08:37 +0200

    1st commit

---
+++ app/src/main/java/b3/mobile/projetandroid/controllers/MainMedicine.java
@@ -0,0 +1,16 @@
+package b3.mobile.projetandroid.controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import b3.mobile.projetandroid.R;
+
+public class MainMedicine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
